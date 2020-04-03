44287545bc8dff1d181fac2f90dfb1b230c59954
srinics <srinics@hotmail.com>
2020-04-03 14:55:23 +0530

    Update Feature2.cpp

--- features/feature2/Feature2.cpp
+++ features/feature2/Feature2.cpp
@@ -1,5 +1,17 @@
+/*
+* Author: M1043833 ( Srinivasan Rajendran )
+* Description: This file (Feature2.cpp) contains simple functions to test dll/so
+*
+* */
+
 #define LIBRARY2_EXPORTS
 
+/*
+Author: M1043833
+Function Name: Feature2
+Description:
+Simple function just print the function function and it has no arguments and no return type
+*/
 #include <iostream>
 #include "Feature2.h"
 extern "C" void LIBRARY_API Feature2(){
