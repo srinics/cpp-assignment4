de9e3d3ef7155e24d9ba1d4eefef8537c82e6dbf
Srini R <srinics@hotmail.com>
2020-04-03 08:26:59 +0000

    completed for linux

--- Main.cpp
+++ Main.cpp
@@ -34,21 +34,22 @@ int main(int argv, char **argc){
 
     try{
         char vSection[8];
-        std::string libName;
+        std::string libName(FEATURE_LIBS);
         std::string apiName;
 
         std::unique_ptr<ConfigFile> c(new ConfigFile(CONFIG_FILE));
 #ifdef _WIN32
         sprintf_s (vSection, "%d.%d", c->GetInteger("Version", "majorNumber", -1), c->GetInteger("Version", "minorNumber", -1) );
         libName = c->GetString(vSection, "libWindows", "");
+      	libName.append(c->GetString(vSection, "libLinux", ""));
 #else
         sprintf (vSection, "%d.%d", c->GetInteger("Version", "majorNumber", -1), c->GetInteger("Version", "minorNumber", -1) );
-        libName = c->GetString(vSection, "libLinux", "");
+      	libName.append(c->GetString(vSection, "libLinux", ""));
 #endif
 
         apiName = c->GetString(vSection, "api", "");
 
-        if(strstr(vSection, "-1") != NULL || libName.length() == 0 || apiName.length() == 0){
+        if(strstr(vSection, "-1") != NULL || libName.length() == strlen(FEATURE_LIBS) || apiName.length() == strlen(FEATURE_LIBS)){
             throw std::runtime_error("Invalid Configuration data....");
         }
         std::cout << "Version " << vSection << std::endl;
