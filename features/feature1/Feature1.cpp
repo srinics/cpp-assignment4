2535440ba241b2fa1d14f0f94e8e0423103b4594
srinics <srinics@hotmail.com>
2020-04-03 14:53:59 +0530

    Update Feature1.cpp

--- features/feature1/Feature1.cpp
+++ features/feature1/Feature1.cpp
@@ -1,8 +1,19 @@
+/*
+* Author: M1043833 ( Srinivasan Rajendran )
+* Description: This file (Feature1.cpp) contains simple functions to test dll/so
+*
+* */
 #define LIBRARY1_EXPORTS
 #include <iostream>
 #include "Feature1.h"
 
 
+/*
+Author: M1043833
+Function Name: Feature1
+Description:
+Simple function just print the function function and it has no arguments and no return type
+*/
 extern "C" void LIBRARY_API Feature1(){
     std::cout << "Feature1 library loaded and Feature1 api exectuting........" << std::endl;
 }
