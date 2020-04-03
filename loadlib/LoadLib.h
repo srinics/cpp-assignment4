c6e182e5ebf36a8243623d04fe36797247a364da
srinics <srinics@hotmail.com>
2020-04-03 09:51:16 +0530

    Create LoadLib.h

---
+++ loadlib/LoadLib.h
@@ -0,0 +1,27 @@
+#ifndef LOADLIB_H
+#define LOADLIB_H
+
+#ifdef _WIN32
+
+#ifdef LIBRARY_EXPORTS
+#define LIBRARY_API __declspec(dllexport)
+#else
+#define LIBRARY_API __declspec(dllimport)
+#endif
+
+#else
+#define LIBRARY_API
+
+#endif
+
+
+
+LIBRARY_API bool LoadLibraryFunc(const char *libPath, const char *funcName);
+#ifdef _WIN32
+LIBRARY_API void LoadDLL(const char*, const char*);
+#else
+LIBRARY_API void LoadSharedObject(const char*, const char*);
+#endif
+
+
+#endif // LOADLIB_H
