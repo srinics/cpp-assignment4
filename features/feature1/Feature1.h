9bfa5b1debd34f770b5039cb36de7078cc681daf
srinics <srinics@hotmail.com>
2020-04-03 14:54:36 +0530

    Update Feature1.h

--- features/feature1/Feature1.h
+++ features/feature1/Feature1.h
@@ -1,3 +1,8 @@
+/*
+* Author: M1043833 ( Srinivasan Rajendran )
+* Description: This file (Feature1.h) contains contains prototype for Feature1.cpp
+*
+* */
 #ifndef FEATURE1_H
 #define FEATURE1_H
 
@@ -14,3 +19,4 @@
 extern "C" void LIBRARY_API Feature1();
 
 #endif // FEATURE1_H
+
